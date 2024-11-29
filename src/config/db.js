1138b9a142f0e204c192a21b9610d22048fc4173
cagatay.yildiz <cagatay@pixupplay.com>
2024-11-29 14:05:52 +0300

    Update start script and configure MongoDB connection using environment variable

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://user_turkcell:kVICxra2OoSXm9y5@cluster0.jcus0vv.mongodb.net/turkcell-e-commerce-db");
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
